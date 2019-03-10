1ae2c212260b8a976dc3053368ab9774f5e3a59d
netnet270 <netnguyen270@gmail.com>
2019-03-10 10:20:18 +0700

    update

--- js/style.js
+++ js/style.js
@@ -1,7 +1,30 @@
 
 $(document).ready(function () {
+
     $(".navigation ul li").click(function(){
-        $(".sub-menu").addClass("open");
+        var subMenu = $(this).children('ul');
+        var classNames = subMenu.attr('class');
+        var itemIconCollapse =  $('.item-icon-collapse');
+
+        console.log(itemIconCollapse.html());
+
+        $('.sub-menu').each(function( index ) {
+            $('.sub-menu').addClass('d-none');
+            itemIconCollapse.html('<i class="far fa-plus-square"></i>');
+           
+        });
+
+		if(classNames.includes('d-none')){		
+			subMenu.removeClass("d-none");
+            subMenu.addClass("d-block");	
+            // subMenu.slideDown(3000);
+            itemIconCollapse.html('<i class="far fa-minus-square"></i>');
+		} else{
+			subMenu.removeClass("d-block");
+            subMenu.addClass("d-none");
+            itemIconCollapse.html('<i class="far fa-plus-square"></i>');
+        }
+        
     })
 
     $(".openbtn").click(function(){
