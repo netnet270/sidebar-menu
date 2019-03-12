4a754fc32920775024f6cbc63fee6ac3258fdd79
nguyen.thi.net <FRAMGIA\nguyen.thi.net@b121787.framgia.com>
2019-03-12 08:59:08 +0700

    update release 2

--- js/style.js
+++ js/style.js
@@ -1,70 +1,68 @@
 $(document).ready(function () {
 
-    $(".navigation ul li:not(:first)").on('click', function () { 
+    $(".navigation ul li:not(:last)").on('click', function () { 
         var subMenu = $(this).children('ul');
-        var classNames = subMenu.attr('class');
         var itemIconCollapse = $(this).find('span:last');
 
         $('.sub-menu').each(function () {
-            $('.sub-menu').slideUp(300);
+            $('.sub-menu').slideUp(400);
             itemIconCollapse.html('<i class="far fa-plus-square"></i>');
         });
 
         if((subMenu).is(":hidden")){
-            subMenu.slideDown(300);
+            subMenu.slideDown(400);
             itemIconCollapse.html('<i class="far fa-minus-square"></i>');
         }
 
         else{
-            subMenu.slideUp(300);
+            subMenu.slideUp(400);
             itemIconCollapse.html('<i class="far fa-plus-square"></i>');
         }
-
     });
 
     var openBtn = $(".openbtn");
     var closeBtn = $(".closebtn");
 
     openBtn.on('click', function () {
+        $('.navigation ul li>a').children('span').removeClass('d-none');
+        $('.navigation ul li.active').children('ul').removeClass('d-none');
+        // $('.navigation ul li.active').children('ul').slideDown(400);
+
         if (parseInt($(window).width()) <= 425) {
             $(".side-bar").css("width", "250px");
             $(".main-content").css("margin-left", "0px");
-            $('.navigation ul li>a').children('span').removeClass('d-none');
             $('.overlay').css({"opacity":"1"});
         }
 
         else {
             $(".side-bar").css("width", "250px");
             $(".main-content").css("margin-left", "250px");
-            $('.navigation ul li>a').children('span').removeClass('d-none');
-            $('.navigation ul li.active').children('ul').removeClass('d-none');
         }
     });
 
     closeBtn.on('click', function () {
+        $('.navigation ul li').children('ul').addClass('d-none');
+        $('.navigation ul li>a').children('span').addClass('d-none');
 
         if (parseInt($(window).width()) <= 425) {
             $(".side-bar").css("width", "60px");
             $(".main-content").css("margin-left", "60px");
-            $('.navigation ul li>a').children('span').addClass('d-none');
-            $('body').removeClass('bg-opacity');
             $('.overlay').css({"opacity":"0"});
         }
 
         else {
             $(".side-bar").css("width", "60px");
             $(".main-content").css("margin-left", "60px");
-            $('.navigation ul li>a').children('span').addClass('d-none');
-            $('.navigation ul li').children('ul').addClass('d-none');
         }
     });
 
     if (parseInt($(window).width()) <= 425) {
         $('.overlay').on('click', function(){         
-            $('.overlay').css({"opacity":"0"});
+            $('.overlay').css({"opacity":"0", " visibility": "hidden"});
             $(".side-bar").css("width", "60px");
             $(".main-content").css("margin-left", "60px");
             $('.navigation ul li>a').children('span').addClass('d-none');
+            $('.navigation ul li').children('ul').addClass('d-none');
         })
     }; 
 
